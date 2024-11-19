7c4f7d94950bc622d88dedcd1d5fc75045a8f89e
Thuc Nguyen <chithuc0605@gmail.com>
2024-11-19 23:24:36 +0700

    Upload bai tap SS2 - 1

---
+++ SS2 - B1.cpp.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	
+	//Khoi tao 2 bien so nguyen
+	
+	int firstNumber=10;
+	int secondNumber=5;	
+	
+	// Tien hanh tao bien va tinh hieu, tich va thuong
+	
+	int a=firstNumber-secondNumber;
+	int b=firstNumber*secondNumber;
+	int c=firstNumber/secondNumber;
+	
+	//Tien hanh in ra va luu ket qua
+	
+	printf("%d %d %d",a,b,c);
+
+	return 0;
+}
