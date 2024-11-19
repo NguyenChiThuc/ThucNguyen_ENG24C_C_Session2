8d19f9ba6ca545d6d0396a92faec94bff8fc7651
Thuc Nguyen <chithuc0605@gmail.com>
2024-11-19 23:25:53 +0700

    Upload bai tap SS2 - 5

---
+++ SS2 - B5.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	//Khoi tao 2 bien chieu dai va chieu rong 
+	
+	int length=2;
+	int width=1;
+	
+	//Tien hanh tinh chu vi va dien tich hinh chu nhat
+	
+	printf("Chu vi hinh chu nhat la %d\n", (length+width)*2);
+	printf("Dien tich hinh chu nhat la %d", length*width);
+	
+	//Tien hanh in ra ket qua 
+	
+	return 0;
+}
