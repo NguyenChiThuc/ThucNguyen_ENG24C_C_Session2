1d3ffcaa89172b1d4b7eeac6d438a0819ae2d669
Thuc Nguyen <chithuc0605@gmail.com>
2024-11-19 23:24:51 +0700

    Upload bai tap SS2 - 2

---
+++ SS2 - B2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	
+	//Khoi tao 2 bien so nguyen
+	
+	int firstNumber=10;
+	int secondNumber=5;	
+	
+	// Tien hanh tinh tong
+	
+	int a=firstNumber+secondNumber;
+	
+	//Tien hanh in ra va luu ket qua
+	
+	printf("%d",a);
+
+	return 0;
+}
