5924c91dbb7bd889286c1bfdba26721c844bf439
Thuc Nguyen <chithuc0605@gmail.com>
2024-11-19 23:25:29 +0700

    Upload bai tap SS2 - 4

---
+++ SS2 - B4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	
+	//Khoi tao bien la 1 canh cua hinh vuong
+	
+	int edge=2;
+	
+	// Tinh chi vi va dien tich hinh vuong
+	
+	printf("Chu vi hinh vuong la %d\n", edge*4);
+	printf("Dien tich hinh vuong la %d", edge*edge);
+	
+	// Tien hanh in ra ket qua
+	
+	return 0;
+}
