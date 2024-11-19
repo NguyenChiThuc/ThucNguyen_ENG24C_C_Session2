3faf32a032c9adb9f3093a6dc8a18a8a788bc983
Thuc Nguyen <chithuc0605@gmail.com>
2024-11-19 23:26:07 +0700

    Upload bai tap SS2 - 6

---
+++ SS2 - B6.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	
+	//Khai bai so PI va kieu du kieu float
+	
+	int pi=3.14;
+	float radius=2;
+	
+	// Tien hanh tinh chu vi va dien tich hinh tron
+	
+	printf("Chu vi hinh tron la %f\n", 2*radius*pi);
+	printf("Dien tich hinh tron la %f", radius*radius*pi);
+	
+	//Tien hanh in ra ket qua
+	
+	return 0;
+	
+}
